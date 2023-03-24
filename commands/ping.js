ece5083b6b2f4e7b1ba4b84ecd729d65a7b8df82
ThaOniFans <valentinoandric200425@gmail.com>
2023-03-24 23:09:36 +0100

    wat opties van reply enz toegevoegd aan ping

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,26 @@
 const { SlashCommandBuilder } = require('discord.js');
+const wait = require('node:timers/promises').setTimeout;
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		// await interaction.reply('Pong!');
+		// await wait(2000);
+		// await interaction.editReply('Pong again!');
+
+		// await interaction.reply({ content: 'Secret Pong!', ephemeral: true });
+
+		// await interaction.followUp('Pong!');
+
+		await interaction.deferReply();
+		await wait(1500);
+		await interaction.editReply('Pong!');
+
+		// const message = await interaction.fetchReply();
+		// console.log(`Fetched reply: ${message.content}`);
+
+		// await interaction.deleteReply();
 	},
 };
